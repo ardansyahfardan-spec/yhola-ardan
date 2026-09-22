37e7c56ac4cd347aedeba40c170ddc46a36c64d3
ardansyahfardan-spec <ardansyahfardan@gmail.com>
2026-09-22 15:22:13 +0700

    Add files via upload

---
+++ undangan.js
@@ -0,0 +1,53 @@
+window.INVITATION = {
+  groom: {
+    fullName: 'Syahfardan Al Hilal Havi',
+    nickname: 'Ardan',
+    parents: 'Hadi Wijanarko & Vinie Nurwachjunie'
+  },
+  bride: {
+    fullName: 'Yholanda Regita Hariyanto',
+    nickname: 'Yhola',
+    parents: 'Sugeng Hariyanto & Anik Rahayu'
+  },
+  event: {
+    date: 'Minggu, 22 November 2026',
+    isoDate: '2026-11-22',
+    venue: 'Estusae Cafe, Trawas, Mojokerto',
+    address: 'Sendang, Penanggungan, Kec. Trawas, Kabupaten Mojokerto, Jawa Timur 61375',
+    maps: 'https://maps.app.goo.gl/j8p8AiPEjb7ygjZT6',
+    ceremony: '08.00 WIB',
+    reception: '09.00 WIB – 13.00 WIB'
+  },
+  about: {
+    story: 'Cerita perjalanan Yhola & Ardan akan ditambahkan di sini. Bagian ini sengaja dibuat mudah diedit ketika cerita kalian sudah siap.',
+    firstMeet: '',
+    relationship: '',
+    proposal: '',
+    wedding: '22 November 2026'
+  },
+  gift: {
+    recipient: 'Syahfardan Al Hilal Havi',
+    bank: 'BCA',
+    account: '6155469025',
+    address: ''
+  },
+  rsvp: {
+    contactName: 'Ardan',
+    whatsapp: '62895399268187',
+    askAttendance: true
+  },
+  copy: {
+    intro: `Dengan memohon rahmat dan ridho Allah SWT, kami bermaksud mengundang Bapak/Ibu/Saudara/i untuk hadir dan turut memberikan doa restu pada acara pernikahan kami.`,
+    verse: `“Dan di antara tanda-tanda kekuasaan-Nya ialah Dia menciptakan untukmu pasangan hidup dari jenismu sendiri, supaya kamu merasa tenteram kepadanya, dan dijadikan-Nya di antaramu rasa kasih dan sayang.”`,
+    verseRef: 'QS. Ar-Rum: 21',
+    closing: `Merupakan kebahagiaan bagi kami apabila Bapak/Ibu/Saudara/i dapat hadir dan memberikan doa restu bagi perjalanan baru kami. Terima kasih atas doa, kasih sayang, dan kehadiran yang diberikan kepada kami.`,
+    inviter: 'Kami yang berbahagia, Yhola & Ardan'
+  },
+  gallery: {
+    photos: []
+  },
+  music: {
+    enabled: true,
+    file: 'assets/music.mp3'
+  }
+};
